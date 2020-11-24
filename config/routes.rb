e5b635f64dbd64f2eb0a6cd941caf185aff3e966
shan tirmizi <tirmizishahnawaz@gmail.com>
2020-11-24 12:16:29 +0000

    router done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :homes do
+    resources :history, only: [ :show]
+    resources :daily, only: [:show]
+    resources :devices, only: [:new, :show, :create ]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
